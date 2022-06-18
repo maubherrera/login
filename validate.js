0a9cd8c6f443c5ce3d8a243aff182021c6790155
maubherrera <mauriciobustosherrera@gmail.com>
2022-06-17 20:16:20 -0500

    first commit

---
+++ validate.js
@@ -0,0 +1,41 @@
+	//Validtion Code For Inputs
+
+let email = document.forms['form']['email'];
+let password = document.forms['form']['password'];
+
+let email_error = document.getElementById('email_error');
+let pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+function validated(){
+	if (email.value.length < 9) {
+		email.style.border = "2px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+	}
+	if (password.value.length < 6) {
+		password.style.border = "2px solid red";
+		pass_error.style.display = "block";
+		password.focus();
+		return false;
+	}
+
+}
+function email_Verify(){
+	if (email.value.length >= 8) {
+		email.style.border = "2px solid green";
+		email_error.style.display = "none";
+		return true;
+	}
+}
+function pass_Verify(){
+	if (password.value.length >= 5) {
+		password.style.border = "2px solid green";
+		pass_error.style.display = "none";
+		return true;
+	}
+}
+
